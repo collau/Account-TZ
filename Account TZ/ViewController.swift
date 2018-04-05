1f74e6ad7dd1f6b930355272811064ac7d815420
JunYi <junyi@alantham-pc.solutionslab.com.sg>
2018-04-05 12:51:58 +0800

    Initial Commit

---
+++ Account TZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Account TZ
+//
+//  Created by JunYi on 5/4/18.
+//  Copyright © 2018 dzhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
